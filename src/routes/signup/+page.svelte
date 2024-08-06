4500ebe93eab36a2bd5f63253fb91fcc95cad61d
Javier Sánchez Mendoza <javier.sanchezm@sat.gob.mx>
2024-08-06 11:35:14 -0600

    feat: sign up was completed

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,18 +1,32 @@
+<script lang="ts">
+  import { enhance } from '$app/forms';
+
+  import type { ActionData } from './$types';
+
+  export let form: ActionData;
+</script>
+
 <section>
-  <form method="post">
+  <form method="post" use:enhance>
     <div class="group">
       <label for="email">Email</label>
-      <input type="email" name="email" id="email" required />
+      <input type="email" name="email" id="email" class="text-black" required />
     </div>
 
     <div class="group">
       <label for="password">Password</label>
-      <input type="password" name="password" id="password" required />
+      <input type="password" name="password" id="password" class="text-black" required />
     </div>
 
     <div class="submit-container">
       <button type="submit">Login</button>
     </div>
+
+    {#if form?.error}
+      <div class="notice error">
+        {form.error}
+      </div>
+    {/if}
   </form>
 
   <div class="actions">
