7a303c9317d8d2bc651e79da6e95e09ab5edc77c
Javier Sánchez Mendoza <javier.sanchezm@sat.gob.mx>
2024-08-06 11:15:46 -0600

    feat: Sign up part was added

---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,21 @@
+<section>
+  <form method="post">
+    <div class="group">
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email" required />
+    </div>
+
+    <div class="group">
+      <label for="password">Password</label>
+      <input type="password" name="password" id="password" required />
+    </div>
+
+    <div class="submit-container">
+      <button type="submit">Login</button>
+    </div>
+  </form>
+
+  <div class="actions">
+    <a href="/signup">Sign Up</a>
+  </div>
+</section>
